84a64e0942114ca7b1744a6d38c799ae43810cbc
pablolospe <yosoypxl@gmail.com>
2024-08-12 12:58:11 -0300

    url filsm

--- components/Films.jsx
+++ components/Films.jsx
@@ -3,20 +3,20 @@ import React from 'react';
 import { formatDate } from '../utils';
 
 const films = async () => {
-  return fetch('http://34.125.251.76:8000/films').then((res) => res.json());
+  return fetch(`${URL}/films`).then((res) => res.json());
 };
 
 async function Films() {
   const data = await films();
   const allFilms = data.data;
-  const filme = allFilms.filter((f) => Number(f._id) < 7);
+  const filme = allFilms.filter((f) => Number(f.id) < 7);
 
   return (
     <>
       {filme?.map((f) => (
         
-        <div className="flex mb-6" key={f._id}>
-          <Link href={`/films/${f._id}`}>
+        <div className="flex mb-6" key={f.id}>
+          <Link href={`/films/${f.id}`}>
             <div 
             className="flex flex-col align-middle text-center m-2 transition-transform" 
             >
@@ -25,7 +25,7 @@ async function Films() {
 
             <img
               className="flex h-80 w-auto mx-5 hover:scale-105 transition-transform ease-in-out"
-              src={`https://starwars-visualguide.com/assets/img/films/${f._id}.jpg`}
+              src={`https://starwars-visualguide.com/assets/img/films/${f.id}.jpg`}
               alt={`Movie cover of '${f.title}'.`}
             />
             
