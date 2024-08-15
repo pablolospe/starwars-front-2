72939424839319aee69e4d8f3551835693dae1d4
pablolospe <yosoypxl@gmail.com>
2024-08-15 15:19:51 -0300

    grid en films

--- app/films/Films.jsx
+++ app/films/Films.jsx
@@ -1,35 +1,35 @@
 import { URL } from '@/config';
 import { getFilms } from '@/utils/fetch';
+import Image from 'next/image';
 import Link from 'next/link';
 
 async function Films() {
   const data = await getFilms();
-  
+
   const allFilms = data;
   const filme = allFilms.filter((f) => Number(f.id) < 7);
 
   return (
-    <>
+    <div className="grid grid-cols-2 sm:grid-cols-3 justify-items-center my-32 mx-4 md:mx-40 gap-8">
       {filme?.map((f) => (
-        
+
         <div className="flex mb-6" key={f.id}>
           <Link href={`/films/${f.id}`}>
-            <div 
-            className="flex flex-col align-middle text-center m-2 transition-transform" 
-            >
-              <h5>{f.title}</h5>
-            </div>
 
-            <img
-              className="flex h-80 w-auto mx-5 hover:scale-105 transition-transform ease-in-out"
+            <Image
+              className="hover:scale-105 transition-transform ease-in-out"
               src={`https://starwars-visualguide.com/assets/img/films/${f.id}.jpg`}
               alt={`Movie cover of '${f.title}'.`}
+              height={400}
+              width={200}
             />
-            
+
+            <h5 className='text-center mt-2'>{f.title}</h5>
+
           </Link>
         </div>
       ))}
-    </>
+    </div>
   );
 }
 
--- app/films/page.jsx
+++ app/films/page.jsx
@@ -1,11 +1,9 @@
 import Films from '@/app/films/Films';
 
-function filmsPage() {
+export default function filmsPage() {
   return (
-    <div className="flex flex-row flex-wrap justify-evenly my-20 mx-4 md:mx-40">
+    <>
       <Films />
-    </div>
+    </>
   );
 }
-
-export default filmsPage;
